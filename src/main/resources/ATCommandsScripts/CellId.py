99093b6cfbe163720c4c9ea1251b322e96120a90
ny3an6 <ndmitrenko38@gmail.com>
2021-06-04 19:26:02 +0300

    some refactoring

--- src/main/resources/ATCommandsScripts/CellId.py
+++ src/main/resources/ATCommandsScripts/CellId.py
@@ -3,8 +3,8 @@ import serial
 
 def getStatus():
     ser = serial.Serial('COM6', 115200, timeout = 1)
-    # cmd = "AT+CPSI?\r"
-    cmd = "AT+COPS=5\r"
+    cmd = "AT+CPSI?\r"
+    # cmd = "AT+COPS=5\r"
     ser.write(cmd.encode())
     # response = ser.read(2)
     response = ser.read(64)
